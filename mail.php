42ef64132389ac77a28f811601e07dbdecc107d5
GregMiras <gregmoiras@gmail.com>
2014-09-13 03:24:16 +0000

    x

--- mail.php
+++ mail.php
@@ -1,15 +1,12 @@
-<?php $name = $_POST['name'];
+<?php
+$name = $_POST['name'];
 $email = $_POST['email'];
+$type = $_POST['type'];
 $message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "gregmoiras@gmail.com"<script type="text/javascript">
-/* <![CDATA[ */
-(function(){try{var s,a,i,j,r,c,l,b=document.getElementsByTagName("script");l=b[b.length-1].previousSibling;a=l.getAttribute('data-cfemail');if(a){s='';r=parseInt(a.substr(0,2),16);for(j=2;a.length-j;j+=2){c=parseInt(a.substr(j,2),16)^r;s+=String.fromCharCode(c);}s=document.createTextNode(s);l.parentNode.replaceChild(s,l);}}catch(e){}})();
-/* ]]> */
-</script>";
+$formcontent=" From: $name \n Type: $type \n Message: $message";
+$recipient = "gregmoiras@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
- 
